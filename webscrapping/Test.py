ad23211f03b25673d00056333e4ea2546326f776
sunnyny-hk <tangsunny99@gmail.com>
2022-03-07 11:50:16 +0800

    iterative version

--- webscrapping/Test.py
+++ webscrapping/Test.py
@@ -1,70 +1,72 @@
-
-from calendar import weekday
 from bs4 import BeautifulSoup
 import logging
 import pandas as pd
-
-# Manually go to website with selected course subject e.g. CSCI
-# Save to local repository to proceed
-# install bs4 package -php install bs4
-
-# read html file 
+import sys
+import os
 
 #==========================================#
 # Replace with suitable file location and name
-filedir="csci3100\CSCI3100\webscrapping\Teaching Timetable.html"
-name= "ACCTdata.json"
+folderdir="webscrapping\html ttb"
+resultdir="webscrapping\data\\"
 #==========================================#
 
-
-
-file = open(filedir).read()
-soup = BeautifulSoup(file,"html.parser")
-table = soup.find(id="gv_detail")
-
 #data init
-course_code, class_nbr, cours_title, credit,teaching_staff,class_type,period,venue,date,weekday =['' for _ in range(10)]
 data_list=[["course_code"],["class_nbr"],["cours_title"],["credit"],["teaching_staff"],["class_type"],["period"],["venue"],["date"],["weekday"]]
 
+def retriveData():
+    file = open(filedir).read()
+    soup = BeautifulSoup(file,"html.parser")
+
+    table = soup.find(id="gv_detail")
+
+    if table == None: 
+        print("no course offering in this semester")
+        return
 
 
-for table in table.find_all('tbody'):
-    rows = table.find_all("tr")
-    for row in rows:
-        try:
-            data = row.find_all('td')
-            course_code = data[0].text
-            class_nbr = data[1].text
-            cours_title = data[2].text
-            credit = data[3].text
-            teaching_staff = data[4].text
-            class_type = data[7].text
-            period = data[10].text[3:]
-            weekday = data[10].text[0:2]
-            venue = data[11].text
-            date = data[12].text
 
-            #skip redundant data 
-            if (class_type=='\xa0'):
-                continue  
+    for table in table.find_all('tbody'):
+        rows = table.find_all("tr")
+        for row in rows:
+            try:
+                data = row.find_all('td')
+                course_code = data[0].text
+                class_nbr = data[1].text
+                cours_title = data[2].text
+                credit = data[3].text
+                teaching_staff = data[4].text
+                class_type = data[7].text
+                period = data[10].text[3:]
+                weekday = data[10].text[0:2]
+                if weekday=="TB":
+                    weekday="TBA"
+                venue = data[11].text
+                date = data[12].text
 
-            data_list[0].append(course_code)
-            data_list[1].append(class_nbr)
-            data_list[2].append(cours_title)
-            data_list[3].append(credit)
-            data_list[4].append(teaching_staff)
-            data_list[5].append(class_type)
-            data_list[6].append(period)
-            data_list[7].append(venue)
-            data_list[8].append(date)
-            data_list[9].append(weekday)
+                raw_data=[course_code,class_nbr,cours_title,credit,teaching_staff,class_type,period,venue,date,weekday]
 
-        except:
-            logging.debug("no td item")
+                #skip redundant data 
+                if (class_type=='\xa0' or class_type=='&nbsp'):
+                    continue  
 
-logging.info("data retrive done")
-print("data retrive done")
-# save to 
-df = pd.DataFrame(data_list)
-df.to_json(name)
+                for i in range(len(raw_data)):
+                    data_list[i].append(raw_data[i])
+                              
 
+            except:
+                logging.debug("no td item")
+
+
+    logging.info("data retrive done")
+    print("data retrive done")
+
+    # save to 
+    df = pd.DataFrame(data_list)
+    df.to_json(name)
+
+
+for filename in os.listdir(folderdir):
+    if filename.endswith(".html"):
+        filedir = os.path.join(folderdir,filename)
+        name = resultdir+filename[0:-5]+".json"
+        retriveData()
