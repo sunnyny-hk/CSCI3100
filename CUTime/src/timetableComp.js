8e2c70faf6bed844a5e14261a3e5c17cb8d38895
sunnyny-hk <tangsunny99@gmail.com>
2022-04-11 20:45:47 +0800

    update table for visibility and background color

--- CUTime/src/timetableComp.js
+++ CUTime/src/timetableComp.js
@@ -5,6 +5,8 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import moment from 'moment'
 import eventBus from "./EventBus";
+import './timetableComp.css'
+import { set } from "date-fns";
 
   const localizer = momentLocalizer(moment);
 
@@ -19,7 +21,7 @@ import eventBus from "./EventBus";
   //TODO: Import data from database
   const events = []
 
-  let CSCI2720 =
+  const CSCI2720 =
   {
     title: "CSCI2720",
     start: new Date("2022-09-05T12:00:00+08:00"),
@@ -27,21 +29,23 @@ import eventBus from "./EventBus";
     name: "CSCI2720: Building Web Application",
     venue: "UCC206",
     lecturer: "Prof xxx",
-    class:"T02",
-    atr: 1
+    class:"Lec",
+    atr: 1,
+    visible: true
   }
   
   
-  let CSCI3100 =
+  const CSCI3100 =
   {
     title: "CSCI3100",
     start: new Date("2022-09-06T12:00:00+08:00"),
     end: new Date("2022-09-06T16:00:00+08:00"),
-    name: "CSCI2720: Building Web Application",
-    venue: "UCC206",
-    lecturer: "Prof xxx",
+    name: "CSCI3100: SoftwareEngineering",
+    venue: "Zoom",
+    lecturer: "Prof ???",
     class:"T02",
-    atr: 2
+    atr: 2,
+    visible: false
   }
 
 function CustomToolbar(){
@@ -52,7 +56,7 @@ function CustomToolbar(){
 
 function Test(props) {
   let testButton = ()=>{
-    eventBus.dispatch('testing',CSCI3100)
+    eventBus.dispatch('testing',[CSCI3100,CSCI2720])
     console.log('testing')
     };
 
@@ -65,15 +69,22 @@ function Test(props) {
 }
 
 function TimeTable (props) {
-    const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" , name:"", venue:"", lecturer:"",class:"",color:""});
     const [allEvents, setAllEvents] = useState(events);
+    const [listEvents, setListEvents] = useState(events);
 
-  
+
+
+    useEffect(()=>{
+      eventBus.on('testing', (data)=>{
+        setListEvents(listEvents=>([...listEvents,data]))
+        setAllEvents(allEvents=>([...allEvents,...data.filter(e=>e.visible)]))
+      })
+      return(eventBus.remove('testing'))
+    },[])
+
+
 
     const handleSelectEvent =   useCallback((e) => window.alert(`Name: ${e.name}\nVenue: ${e.venue}\nLecturer: ${e.lecturer}\nClass: ${e.class}`),[])
-
-
-
 
     function eventPropGetter(event) {
       var style = {
@@ -84,34 +95,97 @@ function TimeTable (props) {
       return {style:style}
     }
 
-    
 
 
-    function handleAddEvent(eve) {
+
+    function readEvent(){
+      //TODO: readevent 
       
-      console.log("pass through handleAddEvent Fn");
-      setAllEvents([...allEvents, eve]);
+      //return(events);
     }
 
-    useEffect(()=>{
-        eventBus.on('testing', (data)=>setAllEvents(allEvents=>([...allEvents,data])))
-        return(eventBus.remove('testing'))
-      },[])
+    function saveEvent(listEvents){
+      //TODO: saveEvent
+    }
 
+
+
+
+    function handleChangeEvent(i,j){
+      let lists=listEvents.slice()
+      let selectedlist=lists[i]
+
+      if (selectedlist[j].visible){
+        selectedlist[j].visible = false
+      }else{
+        selectedlist[j].visible = true
+      }
+
+      if (typeof selectedlist.find(element=>element.visible)=='undefined'){
+        lists.splice(i,1)
+      }
+
+      setAllEvents(lists.flat().filter(e=>e.visible))
+      setListEvents(lists)
+      // let lists=listEvents.slice()
+      // lists.forEach((list,indexi)=>{
+      //   if (index===i){
+      //     if(element.visible===true){
+      //       element.visible= false
+      //     }else{
+      //       element.visible = true
+      //     }
+      //   }
+      // })
+      // setAllEvents(events.filter((e=>e.visible)))
+    }
+
+    function handleChangeColor(i,j){
+      let lists=listEvents.slice()
+      let selectedlist=lists[i]
+      selectedlist[j].atr=(selectedlist[j].atr+1)%6
+
+      setAllEvents(lists.flat().filter(e=>e.visible))
+      setListEvents(lists)
+    }
+
+
+
+    function CourseList(props){
+      let i = props.i;
+      let list = props.list
+      return list.map((element,index)=><CourseCell key={index} event={element} i={i} j={index}/>)
+ 
+      }
+
+    function CourseCell(props){
+      let event=props.event;
+      let i=props.i
+      let j=props.j
+      return (
+
+        <>
+        <>
+        <tr>
+                            <td>{event.name}</td>
+                            <td>{event.venue}</td>
+                            <td>{event.lecturer}</td>
+                            <td>{event.class}</td>
+                            <td><button onClick={()=>handleChangeEvent(i,j)}>on/off</button></td>
+                            <td><button onClick={()=>handleChangeColor(i,j)}>change</button></td>
+                            </tr>
+                            
+                            </>
+
+                            </>
+          
+      )
+    }
 
 
     return (<>
-        {/* This is an temporary function to add event to the time table */}
-        <div>
-            <input type="text" placeholder="Add Title" style={{ width: "20%", marginRight: "10px" }} value={newEvent.title} onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })} />
-            <DatePicker placeholderText="Start Date" style={{ marginRight: "10px" }} selected={newEvent.start} onChange={(start) => setNewEvent({ ...newEvent, start })} />
-            <DatePicker placeholderText="End Date" selected={newEvent.end} onChange={(end) => setNewEvent({ ...newEvent, end })} />
-            <button stlye={{ marginTop: "10px" }} onClick={() => handleAddEvent(newEvent)}>
-                Add Event
-            </button>
-        </div>
 
-        <div>
+        {/* <div>
             <table>
                 <thead>
                     <tr>
@@ -131,11 +205,31 @@ function TimeTable (props) {
                     </tr>
                 </tbody>
             </table>
-        </div>
+        </div> */}
 
         <Calendar localizer={localizer} events={allEvents} defaultView={defaultView} defaultDate={defaultDate} style={{ height: 500, margin: "50px"}} 
         views={{week: true}}  components={{toolbar:CustomToolbar}} min={defaultStartTime} max={defaultEndTime} step={defaultStep} timeslots={2} onSelectEvent={handleSelectEvent} 
         eventPropGetter={event=>eventPropGetter(event)}/>
+
+        <div>
+             <table class ="CourseList">
+                            <thead>
+                            <tr class = "thead">
+                            <th>Course Name</th>
+                            <th>Venue</th>
+                            <th>Lecturer</th>
+                            <th>Class</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {listEvents.map((list, index) => < CourseList i={index} key={index} list={list} />)}
+                            </tbody>
+
+                            
+                            
+            </table>
+                          
+        </div>
     </>);
 }
 
